f2dc017ca88dc842bdf611ac2bfa018788259147
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-10 21:52:00 -0300

    criacao de um programa 'pastotzinho' sobre notas musicais

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("pastorzinho");
+Console.WriteLine("do,ré,mi,fá,fá,fá");
+Console.WriteLine("do,ré,do,ré,ré,ré");
+Console.WriteLine("do,sol,fá,mi,mi,mi");
+Console.WriteLine("do,ré,mi,fá,fá,fá");
